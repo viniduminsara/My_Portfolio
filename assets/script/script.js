d3f3f076858e6023a133025514556471a5b98fd5
viniduminsara <viniduminsara@gmail.com>
2023-09-20 14:55:03 +0530

    complete nav links highlighting

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,3 +5,39 @@ window.addEventListener('load', () => {
         loadingScreen.style.display = 'none';
     }, 2000);
 });
+
+//nav link active
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('nav a');
+
+    function highlightNavLink() {
+        let scrollPos = window.scrollY + 400;
+
+        sections.forEach((section, index) => {
+            if (section.offsetTop <= scrollPos && section.offsetTop + section.offsetHeight > scrollPos) {
+
+                if(index > 3){
+                    navLinks.forEach((link) => {
+                        link.classList.remove('active');
+                    });
+                    navLinks[index-1].classList.add('active');
+                }else if(index < 3){
+                    navLinks.forEach((link) => {
+                        link.classList.remove('active');
+                    });
+                    navLinks[index].classList.add('active');
+                }else{
+                    navLinks.forEach((link) => {
+                        link.classList.remove('active');
+                    });
+                }
+
+            }
+
+        });
+    }
+
+    window.addEventListener('scroll', highlightNavLink);
+});
+
