8869aa15be45dc7d0cec6b3148343e42130a6b62
viniduminsara <viniduminsara@gmail.com>
2023-09-22 20:10:42 +0530

    add scroll animation

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,9 +1,7 @@
 //preloader
 window.addEventListener('load', () => {
-    setTimeout(() => {
-        const loadingScreen = document.getElementById('loading-screen');
-        loadingScreen.style.display = 'none';
-    }, 2000);
+    const loadingScreen = document.getElementById('loading-screen');
+    loadingScreen.style.display = 'none';
 });
 
 //nav link active
@@ -41,6 +39,7 @@ document.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('scroll', highlightNavLink);
 });
 
+//menu button
 const menuBtn = document.querySelector('.menu-btn');
 const navToggle = document.querySelector('.nav_links');
 const navContainer = document.querySelector('.navigation');
@@ -50,4 +49,53 @@ menuBtn.addEventListener('click', () => {
     navToggle.classList.toggle('active');
     navContainer.classList.toggle('active');
 });
+
+//Add scrolling animation
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 1800,
+})
+
+//home section
+ScrollReveal().reveal('.heading, .details', {delay: 300, origin: 'left'});
+ScrollReveal().reveal('.code_area', {delay: 400, origin: 'bottom'});
+ScrollReveal().reveal('.background_img, .social_links', {delay: 600, origin: 'right'});
+
+//about me section
+ScrollReveal().reveal('.about_me_header', {delay : 300, origin: 'bottom'});
+ScrollReveal().reveal('.profile_picture', {delay : 400, origin: 'left'});
+ScrollReveal().reveal('.about_me_details',{delay: 400, origin: 'right'});
+
+//service section
+ScrollReveal().reveal('.service_header div', {delay : 300, origin: 'bottom'});
+ScrollReveal().reveal('.service_header img', {delay : 300, origin: 'left'});
+ScrollReveal().reveal('.services_container .service_item',{delay: 400, origin: 'right', interval: 100});
+
+//skill section
+ScrollReveal().reveal('.skill_header', {delay : 300, origin: 'bottom'});
+ScrollReveal().reveal('.main_skills img',{delay: 400, origin: 'right', interval:200});
+
+//project section
+ScrollReveal().reveal('.project_header div', {delay : 300, origin: 'bottom'});
+ScrollReveal().reveal('.project_header img', {delay : 300, origin: 'left'});
+ScrollReveal().reveal('.project_panel_row:first-child', {delay: 400, origin: 'right'});
+ScrollReveal().reveal('.project_panel_row:last-child', {delay: 400, origin: 'left'});
+ScrollReveal().reveal('#projects a', {delay: 300, origin: 'top'});
+
+//assignment section
+ScrollReveal().reveal('.assignment_header', {delay : 300, origin: 'bottom'});
+ScrollReveal().reveal('.assignment_panel_row .assignment_container',{delay: 400, origin: 'right', interval:100});
+ScrollReveal().reveal('#assignment a', {delay: 300, origin: 'top'});
+
+//contact me section
+ScrollReveal().reveal('.contactme_header', {delay : 300, origin: 'bottom'});
+ScrollReveal().reveal('.contact_form form .form_input',{delay: 300, origin: 'right', interval:100});
+ScrollReveal().reveal('form button',{delay: 300, origin: 'right'});
+ScrollReveal().reveal('#contact_me img',{delay: 300, origin: 'bottom'});
+
+
+
+
+
 
