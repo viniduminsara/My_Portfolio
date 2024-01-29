fcd71ffa6acf2ed3d649bdc0ba0e220bb5c991f1
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-01-29 21:19:56 +0530

    few changes

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,6 +4,53 @@ window.addEventListener('load', () => {
     loadingScreen.style.display = 'none';
 });
 
+//background animation
+document.addEventListener('DOMContentLoaded', () => {
+    //Add scrolling animation
+    ScrollReveal({
+        reset: true,
+        distance: '80px',
+        duration: 1800,
+    });
+
+    //home section
+    ScrollReveal().reveal('.heading, .details', {delay: 300, origin: 'left'});
+    ScrollReveal().reveal('.code_area', {delay: 400, origin: 'bottom'});
+    ScrollReveal().reveal('.background_img, .social_links', {delay: 600, origin: 'right'});
+
+    //about me section
+    ScrollReveal().reveal('.about_me_header', {delay : 300, origin: 'bottom'});
+    ScrollReveal().reveal('.profile_picture', {delay : 400, origin: 'left'});
+    ScrollReveal().reveal('.about_me_details',{delay: 400, origin: 'right'});
+
+    //service section
+    ScrollReveal().reveal('.service_header div', {delay : 300, origin: 'bottom'});
+    ScrollReveal().reveal('.service_header img', {delay : 300, origin: 'left'});
+    ScrollReveal().reveal('.services_container .service_item',{delay: 400, origin: 'right', interval: 100});
+
+    //skill section
+    ScrollReveal().reveal('.skill_header', {delay : 300, origin: 'bottom'});
+    ScrollReveal().reveal('.skills-category-buttons', {delay : 300, origin: 'right', interval:100});
+
+    //project section
+    ScrollReveal().reveal('.project_header div', {delay : 300, origin: 'bottom'});
+    ScrollReveal().reveal('.project_header img', {delay : 300, origin: 'left'});
+    ScrollReveal().reveal('.project_panel_row:first-child', {delay: 400, origin: 'right'});
+    ScrollReveal().reveal('.project_panel_row:last-child', {delay: 400, origin: 'left'});
+    ScrollReveal().reveal('#projects a', {delay: 300, origin: 'top'});
+
+    //assignment section
+    ScrollReveal().reveal('.assignment_header', {delay : 300, origin: 'bottom'});
+    ScrollReveal().reveal('.assignment_panel_row .assignment_container',{delay: 400, origin: 'right', interval:100});
+    ScrollReveal().reveal('#assignment a', {delay: 300, origin: 'top'});
+
+    //contact me section
+    ScrollReveal().reveal('.contactme_header', {delay : 300, origin: 'bottom'});
+    ScrollReveal().reveal('.contact_form form .form_input',{delay: 300, origin: 'right', interval:100});
+    ScrollReveal().reveal('form button',{delay: 300, origin: 'right'});
+    ScrollReveal().reveal('#contact_me img',{delay: 300, origin: 'bottom'});
+});
+
 //nav link active
 document.addEventListener('DOMContentLoaded', () => {
     const sections = document.querySelectorAll('section');
@@ -122,50 +169,6 @@ let typed = new Typed(".typing", {
     loop: true,
 });
 
-//Add scrolling animation
-ScrollReveal({
-    reset: true,
-    distance: '80px',
-    duration: 1800,
-});
-
-//home section
-ScrollReveal().reveal('.heading, .details', {delay: 300, origin: 'left'});
-ScrollReveal().reveal('.code_area', {delay: 400, origin: 'bottom'});
-ScrollReveal().reveal('.background_img, .social_links', {delay: 600, origin: 'right'});
-
-//about me section
-ScrollReveal().reveal('.about_me_header', {delay : 300, origin: 'bottom'});
-ScrollReveal().reveal('.profile_picture', {delay : 400, origin: 'left'});
-ScrollReveal().reveal('.about_me_details',{delay: 400, origin: 'right'});
-
-//service section
-ScrollReveal().reveal('.service_header div', {delay : 300, origin: 'bottom'});
-ScrollReveal().reveal('.service_header img', {delay : 300, origin: 'left'});
-ScrollReveal().reveal('.services_container .service_item',{delay: 400, origin: 'right', interval: 100});
-
-//skill section
-ScrollReveal().reveal('.skill_header', {delay : 300, origin: 'bottom'});
-ScrollReveal().reveal('.skills-category-buttons', {delay : 300, origin: 'right', interval:100});
-
-//project section
-ScrollReveal().reveal('.project_header div', {delay : 300, origin: 'bottom'});
-ScrollReveal().reveal('.project_header img', {delay : 300, origin: 'left'});
-ScrollReveal().reveal('.project_panel_row:first-child', {delay: 400, origin: 'right'});
-ScrollReveal().reveal('.project_panel_row:last-child', {delay: 400, origin: 'left'});
-ScrollReveal().reveal('#projects a', {delay: 300, origin: 'top'});
-
-//assignment section
-ScrollReveal().reveal('.assignment_header', {delay : 300, origin: 'bottom'});
-ScrollReveal().reveal('.assignment_panel_row .assignment_container',{delay: 400, origin: 'right', interval:100});
-ScrollReveal().reveal('#assignment a', {delay: 300, origin: 'top'});
-
-//contact me section
-ScrollReveal().reveal('.contactme_header', {delay : 300, origin: 'bottom'});
-ScrollReveal().reveal('.contact_form form .form_input',{delay: 300, origin: 'right', interval:100});
-ScrollReveal().reveal('form button',{delay: 300, origin: 'right'});
-ScrollReveal().reveal('#contact_me img',{delay: 300, origin: 'bottom'});
-
 //cursor
 const cursor = document.querySelector('.cursor_outline');
 
@@ -256,7 +259,6 @@ function sendMail(){
     }
 }
 
-//background animation
 particlesJS("particles-js",
     {"particles":{"number":{"value":10,"density":{"enable":true,"value_area":800}},
             "color":{"value":"#56317A"},
@@ -264,15 +266,8 @@ particlesJS("particles-js",
             "opacity":{"value":0.1,"random":true,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},
             "size":{"value":100,"random":false,"anim":{"enable":true,"speed":10,"size_min":40,"sync":false}},
             "line_linked":{"enable":false,"distance":200,"color":"#ffffff","opacity":1,"width":2},"move":{"enable":true,"speed":8,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},
-            "interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":false,"mode":"bubble"},"onclick":{"enable":true,"mode":"repulse"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},
-            "bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},
-            "retina_detect":true}
+        "interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":false,"mode":"bubble"},"onclick":{"enable":true,"mode":"repulse"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},
+                "bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},
+        "retina_detect":true}
 );
-
-
-
-
-
-
-
 
